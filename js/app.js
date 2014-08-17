c73e2c9171d7574a69c1abeaf8913f1962e2ba6b
Troy Church <tchurch@berklee.edu>
2014-08-16 20:44:32 -0400

    pressing x for cool pose
    pressing 'x' to strike a cool pose

--- js/app.js
+++ js/app.js
@@ -1,9 +1,28 @@
 $(document).ready(function() {
+	
 	function playHadouken () {
 		$('#hadouken-sound')[0].volume = 0.5;
 		$('#hadouken-sound')[0].load();
 		$('#hadouken-sound')[0].play();
 	}
+
+	$(document).keydown(function(event) {
+		if (event.which == 88) {
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-throwing').hide();
+			$('.ryu-cool').show();
+		}
+	})
+	.keyup(function(event) {
+		if (event.which == 88) {
+			$('.ryu-still').show();
+			$('.ryu-ready').hide();
+			$('.ryu-throwing').hide();
+			$('.ryu-cool').hide();
+		}	
+	});
+
 
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
